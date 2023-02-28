c86693bebc670d7622a1dc22c6f938f273ab9dd4
takesoo <takesooon@gmail.com>
2023-02-28 19:49:44 +0900

    ViewHitCounter

--- cdk-workshop/lib/cdk-workshop-stack.ts
+++ cdk-workshop/lib/cdk-workshop-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from 'aws-cdk-lib'
 import * as lambda from 'aws-cdk-lib/aws-lambda'
 import * as apigw from "aws-cdk-lib/aws-apigateway";
 import { HitCounter } from './hitcounter';
+import { TableViewer } from 'cdk-dynamo-table-viewer';
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -21,6 +22,13 @@ export class CdkWorkshopStack extends cdk.Stack {
     // API Gateway
     new apigw.LambdaRestApi(this, 'Endpoint', {
       handler: helloWithCounter.handler
+    })
+
+    // Table Viewer
+    new TableViewer(this, 'ViewHitCounter', {
+      title: 'Hello Hits',
+      table: helloWithCounter.table,
+      sortBy: '-hits'
     })
   }
 }
